e8662630d0f9225a1a430282f2778c4ba875747a
cpbattrick <48487776+cpbattrick@users.noreply.github.com>
2022-08-08 12:38:51 +0100

    move files

--- test2.js
+++
@@ -1,4 +0,0 @@
-// new array of smurfs, const with length of 10
-const smurfs = ["Brainy Smurf", "Smurfette", "Hefty Smurf", "Lazy Smurf", "Sneaky Smurf", "Brainy Smurf", "Smurfette", "Hefty Smurf", "Lazy Smurf", "Sneaky Smurf"];
-
-// 
